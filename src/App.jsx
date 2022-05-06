369d05af343c91254ce2e16d16b81f0e0a67b824
Vaishnavi Bala <vaishnavi_bala@hotmail.fr>
2022-05-06 09:26:09 +0200

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,16 @@
 import React from 'react';
-
-import logo from './logo.png';
+import Header from './Header';
 import './App.css';
 
 function App() {
   return (
+    <div>
+    <Header />
     <main className="flex flex-col justify-center items-center h-screen">
       <div className="text-center p-4 pb-8">
-        <img src={logo} width="20%" alt="WCS logo" className="my-0 mx-auto" />
-      </div>
-      <div className="text-center p-4 pb-8">
-        <p>Welcome to your fresh, lightweight, React App ! &#127752;</p>
-      </div>
-      <div className="text-center p-4 pb-8">
-        <p>
-          Start in the <code>App.jsx</code> component !
-        </p>
       </div>
     </main>
+    </div>
   );
 }
 
---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    const title = "React Game"
+  return (
+<>
+    <div>Welcome World 🙂</div>
+    <div>{title}</div>
+</>
+  )
+}
+
+export default Header
