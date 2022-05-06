ff9ebb8e45764ec58723e5ed176f8a4bed03c01b
GUILLAUME PINOT <guillaume.pinott@gmail.com>
2022-05-06 11:26:27 +0200

    filter api + button done

--- src/component/GameList.jsx
+++ src/component/GameList.jsx
@@ -1,26 +1,40 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import React from 'react';
 import Game from './Game';
 
 const GameList = () => {
   const [gameList, setGameList] = useState();
-  const url = 'https://apis.wilders.dev/wild-games/games/'
+  const [clicked, setClicked] = useState(false);
+  const url = 'https://apis.wilders.dev/wild-games/games/';
 
   useEffect(() => {
     axios
       .get(url)
       .then((res) => res.data)
-      .then((data) => setGameList(data))
+      .then((data) => setGameList(data));
   }, []);
 
   return (
     <div>
-      <div className='flex justify-center flex-wrap gap-5 mt-10'>
-        {gameList &&
+      <div className="flex justify-center m-5">
+        <button onClick={(e) => setClicked(!clicked)} className="bg-purple-600 p-4 rounded-md flex justify-center text-white">Filter Rating > 4.5 : {clicked ? 'ON' : 'OFF'}</button>
+      </div>
+      <div className="flex justify-center flex-wrap gap-5 mt-8">
+        {clicked ?
+        gameList &&
+          gameList
+            .filter((game) => game.rating > 4.5)
+            .map((game) => (
+              <div key={game.id}>
+                <Game game={game} />
+              </div>
+            ))
+
+        : gameList &&
           gameList.map((game) => (
             <div key={game.id}>
-              <Game game={game}/>
+              <Game game={game} />
             </div>
           ))}
       </div>
