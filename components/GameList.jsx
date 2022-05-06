c018182dd327e4ddc46360c2d8d86f2aee0c84e0
Stefannee <stephanie.caussier@gmail.com>
2022-05-06 11:55:09 +0200

    button filter

--- components/GameList.jsx
+++ components/GameList.jsx
@@ -6,6 +6,11 @@ import Game from './Game';
 
 export const GameList = () => {
   const [games, setGames] = useState([]);
+  const [showRaiting45, setShowRating45] = React.useState(false);
+
+  function handleShowRating45Click() {
+    setShowRating45(!showRaiting45);
+  }
 
   const url = 'https://apis.wilders.dev/wild-games/games/';
 
@@ -15,10 +20,14 @@ export const GameList = () => {
       .then((res) => res.data)
       .then((data) => setGames(data));
   }, []);
+
   return (
     <div>
-      {games &&
-        games.map((game) => (
+      {' '}
+      <button onClick={handleShowRating45Click}>{showRaiting45 ? 'ON' : 'OFF'}Show rating is above 4.5</button>
+      {games
+        .filter((game) => !showRaiting45 || game.rating > 4.5)
+        .map((game) => (
           <div key={game.id}>
             <Game game={game} />
           </div>
