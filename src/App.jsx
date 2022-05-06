f2268fde7380a1775bdf2bb8c49316a5a067c2ac
GUILLAUME PINOT <guillaume.pinott@gmail.com>
2022-05-06 09:30:01 +0200

    Header component & display done

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
 import React from 'react';
-
-import logo from './logo.png';
+import Header from './component/Header';
+import Game from './component/Game';
+import GameList from './component/GameList';
 import './App.css';
 
 function App() {
   return (
-    <main className="flex flex-col justify-center items-center h-screen">
-      <div className="text-center p-4 pb-8">
-        <img src={logo} width="20%" alt="WCS logo" className="my-0 mx-auto" />
-      </div>
-      <div className="text-center p-4 pb-8">
-        <p>Welcome to your fresh, lightweight, React App ! &#127752;</p>
-      </div>
-      <div className="text-center p-4 pb-8">
-        <p>
-          Start in the <code>App.jsx</code> component !
-        </p>
-      </div>
-    </main>
+    <div className='flex justify-center items-center h-10 bg-green-500'>
+      <Header />
+    </div>
   );
 }
 
