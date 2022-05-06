239419a88dfdd2850d806b21d86ecd033481deb5
ACCIPE-RABOT MICKAEL <mickael.accipe.rabot@gmail.com>
2022-05-06 10:48:05 +0200

    btn filter best rated

--- src/components/GameList.jsx
+++ src/components/GameList.jsx
@@ -1,41 +1,49 @@
-import React from 'react'
-import {Game} from './Game'
+import React from 'react';
+import { Game } from './Game';
 import { useState } from 'react/cjs/react.development';
 import { useEffect } from 'react/cjs/react.development';
 import axios from 'axios';
 
 export const GameList = () => {
+  const [games, setGames] = useState([]);
 
-    const [games,setGames]= useState([]);
+  const url = 'https://apis.wilders.dev/wild-games/games/';
 
-    const url = "https://apis.wilders.dev/wild-games/games/"
-    
-    useEffect(()=>{
-        axios
-        .get(url)
-        .then((res)=> res.data)
-        .then((data)=> setGames(data))
-            },[])
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((res) => res.data)
+      .then((data) => setGames(data));
+  }, []);
+
+  const [bestOnly, setBestOnly] = useState(false);
+
+  const onlyTheBest = () => {
+    setBestOnly(!bestOnly);
+    console.log(bestOnly);
+  };
 
   return (
-    <div className='gameList
-    '>
-
+    <div
+      className="gameList
+      flex flex-col justify-center items-center
+    ">
         <h2>GameList</h2>
-        <div className=''>
-            { games &&
-            games
-            .filter((game)=> game.rating >= 4.5)
-            .map((game, index)=>(
+      <button onClick={onlyTheBest}
+      className='btn
+      '
+      >Best rated</button>
+      
+      <div className="">
+        {games &&
+          games
+            .filter((game) => !bestOnly || game.rating > 4.5)
+            .map((game, index) => (
               <div key={index}>
-                  <Game game={game}/>
-              </div>  
+                <Game game={game} />
+              </div>
             ))}
-            
-        </div>
-
-
-
+      </div>
     </div>
-  )
-}
+  );
+};
