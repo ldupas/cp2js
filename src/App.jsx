772ea2e93125c7929d01eea430a84a39f8e70fc9
Stefannee <stephanie.caussier@gmail.com>
2022-05-06 11:03:06 +0200

    update app with component GameList

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { GameList } from '../components/GameList';
 import { Header } from '../components/Header';
 
 import './App.css';
 
 function App() {
   return (
-    <main>
+    <main className="container-primary">
       <Header name="React Games" />
+      <GameList />
     </main>
   );
 }
