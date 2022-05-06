45e8f8a01ef13919123df5824c14fd774c51dac9
ELISA DANILO <elisadanilo2@gmail.com>
2022-05-06 10:28:52 +0200

    wink

--- src/GameList.jsx
+++ src/GameList.jsx
@@ -5,7 +5,12 @@ import { useEffect, useState } from 'react/cjs/react.production.min'
 
 function GameList() {
 
+const [showGoodRating, setShowGoodRating] = useState(false);
 const [gameInfo, setGameInfo] = useState([]);
+
+const BestRating = () => {
+    setShowGoodRating(!showGoodRating);
+};
 
 useEffect(() => {
     axios
@@ -24,9 +29,18 @@ useEffect(() => {
      ))} 
 </div>
 
-    </div>
+<button onClick={BestRating}> Show Best Rating Games Only : {showGoodRating ? 'ON' : 'OFF'} </button>
 
-)});
+<ul>
+{gameInfo.filter((game) => !showGoodRating || game.rating > 4.5)
+.map((element, index) => (
+<Game key={index} name={element.name} rating={element.rating} image={element.background_image} /> 
+     ))} 
+</ul>
+   
+</div>
+
+)})};
 
 
 export default GameList
