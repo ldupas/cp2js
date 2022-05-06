4b182a8980381356a1ee0cf8b51f43edcb4aada1
Eric <pradier_eric@orange.fr>
2022-05-06 12:06:18 +0200

    wink

--- src/components/Gamelist.jsx
+++ src/components/Gamelist.jsx
@@ -7,35 +7,37 @@ import Game from "./Game.jsx";
 // on passe la fonction getID en props afin d'isoler l'id de chacun des héros.
 const Gamelist = () => {
 
-    const [games, setGames] = useState([])
+   const [games, setGames] = useState([])
+   const [filterGames, setFilterGames] = useState(false)
 
-   // GESTION API
+
+    // GESTION API
    const url = "https://apis.wilders.dev/wild-games/games/";
 
    useEffect(() => {
      axios
-       .get(url)
-         .then(res => res.data)
-         .then(data => setGames(data)) 
+     .get(url)
+       .then((res) => res.data)
+       .then((data) => setGames(data));
    }, []);
 
-
 const gamesRating = () => {
-    
+    setFilterGames(!filterGames)
 }
-
+/*
+{games
+    .filter((game) => !showRaiting45 || game.rating > 4.5)
+*/
   return (
     <>
       <div>
+         <button onClick={gamesRating}>Filtrer</button>
         <div>
             { games &&
-            games.map((game) => (
+            games.filter((game) => !filterGames || game.rating > 4.5).map((game) => (
               <Game game={game} />
             ))}
           </div>
-          <button onClick={gamesRating}>
-
-          </button>
       </div>
     </>
   )
