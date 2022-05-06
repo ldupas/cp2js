7b6e6a2347b0217dd23d170c386589a0ad926786
Stefannee <stephanie.caussier@gmail.com>
2022-05-06 09:40:00 +0200

    init component header

---
+++ components/Header.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Header = (reactGame) => {
+  return <div>Ceci est mon checkpoint {reactGame.name} </div>;
+};
--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
+import { Header } from '../components/Header';
 
-import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
-    <main className="flex flex-col justify-center items-center h-screen">
-      <div className="text-center p-4 pb-8">
-        <img src={logo} width="20%" alt="WCS logo" className="my-0 mx-auto" />
-      </div>
-      <div className="text-center p-4 pb-8">
-        <p>Welcome to your fresh, lightweight, React App ! &#127752;</p>
-      </div>
-      <div className="text-center p-4 pb-8">
-        <p>
-          Start in the <code>App.jsx</code> component !
-        </p>
-      </div>
+    <main>
+      <Header name="React Games" />
     </main>
   );
 }
